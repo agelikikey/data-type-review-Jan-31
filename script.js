cb6836f40ef4816a5f832ce9b7a51577e93b101a
agelikikey <agelikikey@gmail.com>
2020-02-01 01:59:39 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let x = 'strings';
+let y = 5 //numbers
+let z = true //booleans
+let w = ['a', 'b', 'c', 'd'] //arrays
+let v = {'one':1, 'two':2} // objects is a key value pair we always want the values we dont really care about the keys but we obvi need them
+
+
+function prints () {
+  console.log('hello')
+}
+// you must call the function in order for it to print.. Here we have just defined the function in order to call the function it will look like this 
+
+// prints(); there is also a different syntax it looks like this let prints = () => {
+//   console.log('hello')
+// }
+
+let prints = (parameters) => {
+  console.log(parameters)
+}
+prints('hell');
+prints('o');
+prints(4);
+prints( [4,5,6,7]);
+
+
+//console.log( w[3] )
+// console.log( v.one )
+// console.log (['a', 'b', 'c', 'd'] [{'one':1, 'two':2}.one] )
+
